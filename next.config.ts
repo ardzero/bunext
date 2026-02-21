35c3f9e101f890101bafbd9db3dcc5365533146a
ardzero <farhanchowdhury6543@gmail.com>
2026-02-21 17:44:44 +0600

    next config change

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true, // 'annotation' to make it opt in https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler#annotations
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'v0.dev', pathname: '/placeholder.svg' },
+    ],
+  },
+  compiler:
+    process.env.NODE_ENV === "production" ? { removeConsole: true } : {},
 };
 
 export default nextConfig;
