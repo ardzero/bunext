838616704c4840d2a5c51551f4f609a3b4a895fd
ardzero <farhanchowdhury6543@gmail.com>
2026-02-24 20:34:10 +0600

    image component added

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -275,3 +275,25 @@ export const decodeBase64 = (encodedStr: string): string => {
         return ''
     }
 }
+
+// image comp utils
+export const shimmer = (w: number | `${number}`, h: number | `${number}`) => {
+    const color = { center: '#efefef', side: '#dfdfdf' }
+    return `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="${color.side}" offset="20%" />
+      <stop stop-color="${color.center}" offset="50%" />
+      <stop stop-color="${color.side}" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="${color.side}" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+}
+
+// converts smv to base64
+export const svgToBase64 = (str: string) =>
+    isSSR ? Buffer.from(str).toString('base64') : window.btoa(str)
